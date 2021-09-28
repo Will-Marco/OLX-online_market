33cf091027d316ae7c4393335bf97c6d01a48fe3
Will Marco <mr.marco0101@gmail.com>
2021-09-28 19:29:48 +0500

    middlewares and settings adjusted

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
 require("dotenv").config;
+
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
+
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
+async function server() {
+  const app = express();
+  // middlewares
+  app.use(express.json);
+  app.use(express.urlencoded());
+  app.use(morgan("dev"));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "src", "public")));
+  // settings
+  app.set("view engine", "ejs");
+}
